ef8638211acac0ba92485c5c49ad88da6dc5eb97
gdcp <gdcp@m25.local>
2018-06-07 11:13:14 +0800

    1234

--- 6.7/ViewController.swift
+++ 6.7/ViewController.swift
@@ -7,12 +7,52 @@
 //
 
 import UIKit
-
+import MapKit
 class ViewController: UIViewController {
-
+    var mapView : MKMapView?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mapView = MKMapView(frame: self.view.frame)
+        mapView?.mapType = .standard
+        mapView?.region = MKCoordinateRegion(center: CLLocationCoordinate2DMake(23.1825, 113.3538), span: MKCoordinateSpanMake(1,1 ))
+            mapView?.showsScale = true
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2DMake(23.185, 113.3538)
+        annotation.title = "GDCP"
+        annotation.subtitle = "GuangZhou"
+        mapView?.addAnnotation((annotation))
+        self.view.addSubview(mapView!)
+        
+        let fromcoor = CLLocationCoordinate2DMake(23.1825, 113.3588)
+        let tocoor = CLLocationCoordinate2DMake(24.55, 116.1)
+        let fromPlace = MKPlacemark(coordinate: fromcoor)
+        let toPlace = MKPlacemark(coordinate: tocoor)
+        let fromItem = MKMapItem(placemark: fromPlace)
+        let toItem = MKMapItem(placemark: toPlace)
+        
+        let request = MKDirectionsRequest()
+        request.source = fromItem
+        request.destination = toItem
+        request.requestsAlternateRoutes = true
+        
+        let direction = MKDirections(request: request)
+        direction.calculate {(resonse , error) in
+            if error != nil{
+                print(error!)
+            }else {
+                let route = resonse?.routes[0]
+                let steps = route?.steps
+                for step in steps!{
+                    let anno = MKPointAnnotation()
+                    anno.coordinate=step.polyline.coordinate
+                    anno.title = step.instructions
+                    anno.subtitle = step.notice
+                    self.mapView?.addAnnotation(anno)
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
